fbc4dd442a529239054309eaf24324798c0d1d65
Chihcheng Hsieh <37566901+ChihchengHsieh@users.noreply.github.com>
2021-08-03 18:29:30 +1000

    inint

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Divider, Image } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Security Web Page</h1>
+      <Divider></Divider>
+      <Image src="https://sectigostore.com/blog/wp-content/uploads/2020/06/cyber-security-tips-for-smbs.jpg" preview={false}></Image>
+      <div>Empty</div>
     </div>
   );
 }
